2fe72e503825a0d2fea47fe52f7144c30246b4e1
didier-bazayesu <didierbazayesu@gmail.com>
2026-02-17 17:25:26 +0200

    feat: rendering navBar component

--- src/components/NavbarMeme.jsx
+++ src/components/NavbarMeme.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function NavbarMeme() {
   return (
-    <div>NavbarMeme</div>
+      <section className='bg-[#9125b7] p-5 text-white text-xl font-extrabold '>
+
+        <div className='flex  gap-x-2'>
+            <img src="./image/Troll Face.png" alt="memeImage" className='w-8 h-8' />
+            <span>Meme Generator</span>
+
+        </div >
+    </section>
   )
 }
 
