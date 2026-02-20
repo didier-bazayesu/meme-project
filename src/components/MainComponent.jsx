cdaa572579ae523d01ba82f4d319ac36c330f095
didier-bazayesu <didierbazayesu@gmail.com>
2026-02-20 20:27:26 +0200

    feat: rendering random images

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -1,35 +1,50 @@
 import MordorMeme from "./MemeImage"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
 
-export default function Main() {
-    let[input1, setinput1] = useState('');
-    let [input2, setinput2] = useState('');
+
+export default function MainComponent() {
+
+let[showMeme, setShowMeme] = useState(null);
     const [meme, setMeme] = useState({
         topText: "One does not simply",
         bottomText: "Walk into Mordor",
         imageUrl: "http://i.imgflip.com/1bij.jpg"
     })
+    useEffect(()=>{
+         fetch("https://api.imgflip.com/get_memes").then(resp=> resp.json())
+         .then(data=> console.log(setShowMeme(data.data.memes)));
+    },[])
+
+
 
    //handling input 1
     function handleInput1 (event){
-          setMeme(prev=>({
-            ...prev,
-              topText:event.target.value
+       let{name,value}=event.target
+        
+       setMeme(prev=> ({
+        ...prev,
+        [name] : value
 
-          }))
+       }))
+
+         
     }
 
-    //handling inpu2
-
-    function handleInput2(event) {
-        setMeme(prev => ({
+    //get meme Image
+    function getRandomMemeImage(){
+        const random = Math.floor(Math.random() * 100)
+        const url = showMeme[random].url
+        // const image = showMeme.filter((_,i)=> i == random);
+        setMeme(prev=> ({
             ...prev,
-            bottomText: event.target.value
+            imageUrl: url
 
         }))
     }
+
+   
 
 
 
@@ -59,21 +74,26 @@ export default function Main() {
                             className="border-gray-200 border max-w-sm rounded-sm text-center py-2 "
                             type="text"
                             placeholder="Walk into monitor ..."
-                            name="topText"
+                            name="bottomText"
                             value={meme.bottomText}
-                            onChange={handleInput2}
+                            onChange={handleInput1}
                         />
                     </label>
                 </div>
 
             </section>
             <div className="flex ">
-                <button className="bg-[#9125b7] text-white mt-10 mx-auto w-xl p-2 rounded-[5px] cursor-pointer ">Get a new meme image 🖼</button>
+                <button
+                 className="bg-[#9125b7] text-white mt-10 mx-auto w-xl p-2 rounded-[5px] cursor-pointer "
+                 onClick={getRandomMemeImage}
+                >Get a new meme image 🖼
+                </button>
 
             </div>
 
             {/* making  image */}
             <MordorMeme meme={meme}/>
+          
            
         </main>
     )
