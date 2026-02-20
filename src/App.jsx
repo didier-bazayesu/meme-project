01447574d165b06f382b491d233e4729786154f5
didier-bazayesu <didierbazayesu@gmail.com>
2026-02-20 20:28:15 +0200

    feat: App.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 
-import Main from "./components/MainComponent"
+import axios from "axios"
+import MainComponent from "./components/MainComponent"
 import NavbarMeme from "./components/NavbarMeme"
+import React, { useEffect } from "react"
 
 
 function App() {
+  const [starWarsData, setStarWarsData] = React.useState(null)
+  console.log("rendered")
+  async function fetchData() {
+    const data =await axios.get('https://swapi.dev/api/people/1');
+    setStarWarsData(data);
+  }
+  useEffect(()=>{
+      fetchData();
+  },[])
+  
   return (
+
     <div className="" >
+
       <NavbarMeme/>
-      <Main/>
+      <MainComponent/>
     </div>
   )
 }
